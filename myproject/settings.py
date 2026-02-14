c492d9ace7325f01c0373b4e232e4e3dff8e4111
shankar117512 <shankar117512@gmail.com>
2026-02-14 12:07:57 +0000

    CSRF added

--- myproject/settings.py
+++ myproject/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ["https://ubuntu-postgresql123-production.up.railway.app"]
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ubuntu-postgresql123-production.up.railway.app']
 
 
 # Application definition
